cacc865d8e159ec370b50b782a48a859606645d0
Dhen Padilla <dhenepadilla@gmail.com>
2020-06-20 02:01:18 +0100

    🛬 Finished landing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class Home extends Component {
     render() {
         return (
-            <h1>
-                Welcome to Rendezvous, {this.props.name}!
-            </h1>
+            <div className="block h-full w-screen pl-48 pt-40 items-center justify-between">
+                <h1>
+                    <span className="font-sofia font-bold text-6xl">
+                        MEET
+                        &#x26;
+                        DISCOVER
+                    </span>
+                </h1>
+                <h4 className="font-sofia font-light text-xl w-1/3">
+                    Rendezvous provides an efficient method to meet up with 
+                    friends, removing the unnecessary clutter when organising meetups.
+                </h4>
+                <button href="/" className="bg-black block lg:inline-block lg:mt-12 text-white hover:bg-transparent hover:border-transparent hover:text-black border-solid border border-black rounded py-3 px-6 mt-4 lg:inline-block lg:mt-0 text-black hover:text-teal mr-4 font-sofia font-normal tracking-wider">
+                    sign up
+                </button>
+            </div>
         )
     }
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,11 +4,11 @@ function OutsideNavigation() {
     return (
         <nav className="flex items-center justify-between flex-wrap bg-white-500 p-12">
             <div className="block lg:hidden">
-                <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+                <button className="flex items-center px-3 py-2 border rounded text-black border-black hover:text-white hover:border-white hover:bg-black">
                     <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
                 </button>
             </div>
-            <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+            <div className="w-full block md:hidden flex-grow lg:flex lg:items-center lg:w-auto">
                 <div className="text-md lg:flex-grow">
                     <button href="/" className="bg-transparent float-right block mt-4 lg:inline-block lg:mt-0 text-black hover:bg-black hover:border-transparent hover:text-white border-solid border border-black rounded py-3 px-6 mt-4 lg:inline-block lg:mt-0 text-black hover:text-teal mr-4 font-sofia font-normal tracking-wider">
                         sign up
@@ -23,11 +23,8 @@ function OutsideNavigation() {
                         about
                     </a>
                 </div>
-            <div>
-                <button href="/" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</button>
-             </div>
-        </div>
-</nav>
+            </div>
+        </nav>
     )
 }
 
